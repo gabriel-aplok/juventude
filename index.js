aaedf0e119762554499feae2dabbaaa2940ba644
Gabriel Aplok <97059054+gabriel-aplok@users.noreply.github.com>
2023-10-01 18:54:15 -0300

    working in the design lol

---
+++ index.js
@@ -0,0 +1,33 @@
+const header = document.getElementById("header");
+
+window.onscroll = function () {
+    scrollIndicator();
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cookieConsent = document.querySelector(".cookie-consent");
+    const cookieConsentButton = document.querySelector(".cookie-consent-button");
+
+    if (localStorage.getItem("cookie-consent")) {
+        cookieConsent.style.display = "none";
+    } else {
+        cookieConsent.style.display = "block";
+    }
+
+    cookieConsentButton.addEventListener("click", () => {
+        localStorage.setItem("cookie-consent", true);
+        cookieConsent.style.display = "none";
+    });
+});
+
+function headerMenu() {
+    header.classList.toggle("open");
+}
+
+function scrollIndicator() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+
+    document.getElementById("scroll-indicator-bar").style.width = scrolled + "%";
+}
